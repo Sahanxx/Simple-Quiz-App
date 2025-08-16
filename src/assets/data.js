326474b24a5a71ee2f9d0f13c6df4932fca3fea9
Sahan Anjana Hewage <sahananjana63@gmail.com>
2025-08-16 11:46:00 +0000

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "What is the capital of France?",
+    option1: "Berlin",
+    option2: "Madrid",
+    option3: "Paris",
+    option4: "Rome",
+    ans: 3,
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    option1: "Earth",
+    option2: "Mars",
+    option3: "Jupiter",
+    option4: "Venus",
+    ans: 2,
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    option1: "Atlantic Ocean",
+    option2: "Indian Ocean",
+    option3: "Arctic Ocean",
+    option4: "Pacific Ocean",
+    ans: 4,
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    option1: "Vincent van Gogh",
+    option2: "Pablo Picasso",
+    option3: "Leonardo da Vinci",
+    option4: "Claude Monet",
+    ans: 3,
+  },
+  {
+    question: "What is the chemical symbol for water?",
+    option1: "O2",
+    option2: "H2O",
+    option3: "CO2",
+    option4: "NaCl",
+    ans: 2,
+  },
+];
